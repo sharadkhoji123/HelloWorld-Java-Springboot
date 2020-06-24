9d760742696981ce05418252e963ae765e8f3843
root <root@testvm.4sh2anurukvebf1yrhbpyi02ma.bx.internal.cloudapp.net>
2020-06-24 17:50:41 +0000

    build file added

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker container run --rm -it -v ..:/app -w /app maven:3-jdk-11 mvn -B -DskipTests clean package
+docker container run --rm -v ..:/app -w /app maven:3-jdk-11 mvn -B -DskipTests clean package
